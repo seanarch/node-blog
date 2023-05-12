e15a19bf3b572bc94e3ea03896351d377f9e635d
seanarch <zhuqingyun@outlook.com>
2023-05-12 10:09:11 -0600

    Added pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,14 +5,30 @@ const Post = require("../models/Post");
 // Routes
 // GET / HOME
 router.get("", async (req, res) => {
-  const locals = {
-    title: "NodeJS Blog",
-    description: "Simple blog created with NodeJS and mongoDB",
-  };
-
   try {
-    const data = await Post.find();
-    res.render("index", { locals, data });
+    const locals = {
+      title: "NodeJS Blog",
+      description: "Simple blog created with NodeJS and mongoDB",
+    };
+
+    let perPage = 6;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.count();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
   } catch (err) {
     console.error(err);
   }
