870ae0cb95482eb0faba8b4a86ef26804042bb9c
seanarch <zhuqingyun@outlook.com>
2023-05-17 10:27:27 -0600

    Added user register method

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const adminLayout = "../views/layouts/admin";
 
@@ -32,6 +34,28 @@ router.post("/admin", async (req, res) => {
       res.redirect("/admin");
     } else {
       res.send("Wrong username or password");
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// Routes
+// POST / admin - register
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: "User created", user });
+    } catch (err) {
+      if (err.code === 11000) {
+        res.status(409).json({ message: "User already in use" });
+      }
+      res.status(500).json({ message: "Internal server error" });
     }
   } catch (err) {
     console.error(err);
